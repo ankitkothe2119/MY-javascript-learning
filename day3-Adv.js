84640bed38b091e97544eea545c9da1b4cd780e0
ankitkothe2119 <135228960+ankitkothe2119@users.noreply.github.com>
2023-08-11 00:11:48 +0530

    Create day3-Adv.js

---
+++ day3-Adv.js
@@ -0,0 +1,16 @@
+// how to copy reference valuse spread opreter [...]
+ let a=[1,2,3,4,5];
+ let b=[...a];
+ console.log(b);
+ console.log(a);
+ console.log("b"+b.pop());
+ console.log("a="+a);
+ console.log("b="+b);
+ //as same as object
+ let x={name:"ankit",lastname:"kothe"}
+ console.log(x);
+ let y={...x}
+ console.log(y);
+ 
+ 
+ //foreach loop 
